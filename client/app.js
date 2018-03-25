ed12df4aad7386f5674c7bebf39e9687aa0b8daf
Chiranjeevi <jeevi146@gmail.com>
2018-03-25 02:08:10 -0400

    added Form to add a new book

--- client/app.js
+++ client/app.js
@@ -1,28 +1,28 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(function($routeProvider){
+myApp.config(function($routeProvider) {
 
-	$routeProvider.when('/', {
-		controller: 'BooksController',
-		templateUrl: 'views/books.html'
-	})
-	.when('/book', {
-		controller: 'BooksController',
-		templateUrl: 'views/books.html'
-	})
-	.when('/book/details/:id', {
-		controller: 'BooksController',
-		templateUrl: 'views/book_details.html'
-	})
-	.when('/book/add', {
-		controller: 'BooksController',
-		templateUrl: 'views/add_book.html'
-	})
-	.when('/book/edit/:id', {
-		controller: 'BooksController',
-		templateUrl: 'views/edit_book.html'
-	})
-	.otherwise({
-		redirectTo:'/'
-	});
+  $routeProvider.when('/', {
+      controller: 'BooksController',
+      templateUrl: 'views/books.html'
+    })
+    .when('/book', {
+      controller: 'BooksController',
+      templateUrl: 'views/books.html'
+    })
+    .when('/book/details/:id', {
+      controller: 'BooksController',
+      templateUrl: 'views/book_details.html'
+    })
+    .when('/book/add', {
+      controller: 'BooksController',
+      templateUrl: 'views/add_book.html'
+    })
+    .when('/book/edit/:id', {
+      controller: 'BooksController',
+      templateUrl: 'views/edit_book.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 });
--- client/controllers/books.js
+++ client/controllers/books.js
@@ -1,16 +1,24 @@
 var myApp = angular.module('myApp');
 
-myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
-	console.log('BooksController loaded');
-	$scope.getBooks = function(){
-		$http.get('/api/book').success(function(response){
-			$scope.books = response;
-		});
-	}
-	$scope.getBook = function(){
-		var id = $routeParams.id;
-		$http.get('/api/book/'+id).success(function(response){
-			$scope.book = response;
-		});
-	}
+myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
+  console.log('BooksController loaded');
+  $scope.getBooks = function() {
+    $http.get('/api/book').success(function(response) {
+      $scope.books = response;
+    });
+  }
+
+  $scope.getBook = function() {
+    var id = $routeParams.id;
+    $http.get('/api/book/' + id).success(function(response) {
+      $scope.book = response;
+    });
+  }
+
+  $scope.addBook = function() {
+    $http.post('/api/book/', $scope.book).success(function(response) {
+      window.location.href = '#/book';
+    });
+  }
+
 }]);
