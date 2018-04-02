64d36ec204a6c2f1be55ab3211c29fbe70eecbee
Chiranjeevi <jeevi146@gmail.com>
2018-04-01 21:44:27 -0400

    Added listing of Genres

--- client/controllers/books.js
+++ client/controllers/books.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('myApp');
 
 myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
-//  console.log('BooksController loaded');
+
   $scope.getBooks = function() {
     $http.get('/api/book').success(function(response) {
       $scope.books = response;
---
+++ client/controllers/genres.js
@@ -0,0 +1,11 @@
+var myApp = angular.module('myApp');
+
+myApp.controller('GenresController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
+
+  $scope.getGenres = function() {
+    $http.get('/api/genre').success(function(response) {
+      $scope.genres = response;
+    });
+  }
+
+}]);
