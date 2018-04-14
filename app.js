d97a7a2f6a0fc62a98cee6570e0599b159f84f71
Chiranjeevi <jeevi146@gmail.com>
2018-04-13 23:49:21 -0400

    move routes to separate files

--- app.js
+++ app.js
@@ -3,143 +3,32 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var dotenv = require('dotenv');
+const bookRoute = require('./routes/books-route');
+const genreRoute = require('./routes/genres-route');
 
 //Using dot env to laod environment variables
 dotenv.load();
 
 //BOOKSTORE_MLAB_URL - MongoDb url from Mlab
-const MONGODB_URL= process.env.BOOKSTORE_MLAB_URL || 'mongodb://localhost/bookstore';
+const MONGODB_URL = process.env.BOOKSTORE_MLAB_URL || 'mongodb://localhost/bookstore';
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 //location for static content
 app.use(express.static(__dirname + '/client'));
 
-Genre = require('./models/genre');
-Book = require('./models/book');
-
 //Use Mlab url here
 mongoose.connect(MONGODB_URL);
 
 var db = mongoose.connection;
 
-app.get('/', function(req, res) {
-  res.send('Please Use the API /api/book or /api/genre');
+app.get('/', function (req, res) {
+	res.send('Please Use the API /api/book or /api/genre');
 });
 
-//////////////////// GENRE API ////////////////////////
-
-//To get all the genres from database
-app.get('/api/genre', function(req, res) {
-  Genre.getGenres(function(err, genres) {
-    if (err) {
-      throw err;
-    }
-    res.json(genres);
-  });
-});
-
-//To get a genre details using it's id
-app.get('/api/genre/:_id', function(req, res) {
-  Genre.getGenreById(req.params._id, function(err, genre) {
-    if (err) {
-      throw err;
-    }
-    res.json(genre);
-  });
-});
-
-//To add a new Genre to the database
-app.post('/api/genre', function(req, res) {
-  var genre = req.body;
-  Genre.addGenre(genre, function(err, genre) {
-    if (err) {
-      throw err;
-    }
-    res.json(genre);
-  });
-});
-
-//To update a genre details by its id
-app.put('/api/genre/:_id', function(req, res) {
-  var id = req.params._id;
-  var genre = req.body;
-  Genre.updateGenre(id, genre, {}, function(err, genre) {
-    if (err) {
-      throw err;
-    }
-    res.json(genre);
-  });
-});
-
-//To delete a genre using its id
-app.delete('/api/genre/:_id', function(req, res) {
-  var id = req.params._id;
-  Genre.deleteGenre(id, function(err, genre) {
-    if (err) {
-      throw err;
-    }
-    res.json(genre);
-  });
-});
-
-
-//////////////////// BOOK API ////////////////////////
-
-// To get all the books from the database
-app.get('/api/book', function(req, res) {
-  Book.getBooks(function(err, books) {
-    if (err) {
-      throw err;
-    }
-    res.json(books);
-  });
-});
-
-//To get a book details using it's id
-app.get('/api/book/:_id', function(req, res) {
-  Book.getBookById(req.params._id, function(err, book) {
-    if (err) {
-      throw err;
-    }
-    res.json(book);
-  });
-});
-
-//To add a new book to the database
-app.post('/api/book', function(req, res) {
-  var book = req.body;
-  Book.addBook(book, function(err, book) {
-    if (err) {
-      throw err;
-    }
-    res.json(book);
-  });
-});
-
-//To update a book details by its id
-app.put('/api/book/:_id', function(req, res) {
-  var id = req.params._id;
-  var book = req.body;
-  Book.updateBook(id, book, {}, function(err, book) {
-    if (err) {
-      throw err;
-    }
-    res.json(book);
-  });
-});
-
-//To delete a book using its id
-app.delete('/api/book/:_id', function(req, res) {
-  var id = req.params._id;
-  Book.deleteBook(id, function(err, book) {
-    if (err) {
-      throw err;
-    }
-    res.json(book);
-  });
-});
+app.use('/api/book', bookRoute);
+app.use('/api/genre', genreRoute);
 
 app.listen(PORT);
 console.log('Started BookStore application on port ' + PORT);
-console.log('Open http://localhost:'+ PORT +' on your browser!');
+console.log('Open http://localhost:' + PORT + ' on your browser!');
